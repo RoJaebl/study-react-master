29ae0431f22749d79723f8e598902f08a4745a6c
RoJaebl <qgs0924@gmail.com>
2023-02-14 09:25:29 +0900

    :hammer: #1.4 Theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl);
 
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColer: "#111",
+};
+const lightTheme = {
+    textColor: "#111",
+    backgroundColer: "whitesmoke",
+};
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={darkTheme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
