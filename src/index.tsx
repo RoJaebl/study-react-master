180d9bd8e74713dfb71ae77a80e1ea8a8b90211b
RoJaebl <qgs0924@gmail.com>
2023-02-14 22:33:23 +0900

    :hammer: #2.2 Typing the props

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl as Element);
 
-const darkTheme = {
-    textColor: "whitesmoke",
-    backgroundColer: "#111",
-};
-const lightTheme = {
-    textColor: "#111",
-    backgroundColer: "whitesmoke",
-};
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={darkTheme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
