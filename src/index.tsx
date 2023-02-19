284aed797a3351678ab01ebbbbc1dadc20ec105d
RoJaebl <qgs0924@gmail.com>
2023-02-20 00:14:44 +0900

    :hammer: #4.1 styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import router from "./routes/router";
+import { theme } from "./theme";
 
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl as Element);
 
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </React.StrictMode>
 );
