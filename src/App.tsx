cab4179585bb5a0ff64c6939658c71936c4e829a
RoJaebl <qgs0924@gmail.com>
2023-03-15 17:06:33 +0900

    :fire: initial motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { motion } from "framer-motion";
+import "./styles.scss";
+
 function App() {
-    return null;
+    return (
+        <motion.div
+            className="container"
+            initial={{ scale: 0 }}
+            animate={{ rotate: 180, scale: 1 }}
+            transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+            }}
+        />
+    );
 }
 
 export default App;
