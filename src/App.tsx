77bb99418eeb44da8f5dc899804c2ea46d5240cd
RoJaebl <qgs0924@gmail.com>
2023-03-16 23:10:29 +0900

    :hammer: #7.4 Gestures 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -4,59 +4,34 @@ import styled from "styled-components";
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    background-color: arga(255, 255, 255, 0.2);
-    border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
-`;
-const Circle = styled(motion.div)`
     background-color: white;
-    height: 70px;
-    width: 70px;
-    place-self: center;
-    border-radius: 50%;
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
 `;
 
 const boxVariants: Variants = {
-    start: {
-        opacity: 0,
-        scale: 0.5,
+    hover: {
+        scale: 1.5,
+        rotateZ: 90,
+        boxShadow:
+            "2px 0px 3px rgba(0, 0, 0, 0.1), 10px 0 20px rgba(0, 0, 0, 0.5)",
     },
-    end: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            type: "spring",
-            duration: 1,
-            bounce: 0.5,
-            delayChildren: 0.3,
-            staggerChildren: 0.1,
-        },
+    down: { scale: 1, borderRadius: "100px" },
+    dragging: {
+        backgroundColor: "rgb(46, 204, 133)",
+        transition: { duration: 1 },
     },
 };
-const circleVariants: Variants = {
-    start: {
-        opacity: 0,
-        y: 10,
-    },
-    end: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.8,
-        },
-    },
-};
+
 function App() {
     return (
-        <Box variants={boxVariants} initial="start" animate="end">
-            <Circle variants={circleVariants} />
-            <Circle variants={circleVariants} />
-            <Circle variants={circleVariants} />
-            <Circle variants={circleVariants} />
-        </Box>
+        <Box
+            drag
+            variants={boxVariants}
+            whileHover="hover"
+            whileTap="down"
+            whileDrag="dragging"
+        />
     );
 }
 
