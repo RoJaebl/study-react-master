bfe885585f0413c17823ba0e3062c258682ba59c
RoJaebl <qgs0924@gmail.com>
2023-03-17 20:57:19 +0900

    :hammer: #7.4 Gestures 2/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { motion, Variants } from "framer-motion";
+import { useRef } from "react";
 import styled from "styled-components";
 
 const Box = styled(motion.div)`
-    width: 200px;
-    height: 200px;
+    width: 100px;
+    height: 100px;
     background-color: white;
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
+`;
+const BiggerBox = styled.div`
+    width: 400px;
+    height: 400px;
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const boxVariants: Variants = {
@@ -17,21 +27,22 @@ const boxVariants: Variants = {
             "2px 0px 3px rgba(0, 0, 0, 0.1), 10px 0 20px rgba(0, 0, 0, 0.5)",
     },
     down: { scale: 1, borderRadius: "100px" },
-    dragging: {
-        backgroundColor: "rgb(46, 204, 133)",
-        transition: { duration: 1 },
-    },
 };
 
 function App() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
-        <Box
-            drag
-            variants={boxVariants}
-            whileHover="hover"
-            whileTap="down"
-            whileDrag="dragging"
-        />
+        <BiggerBox ref={biggerBoxRef}>
+            <Box
+                drag
+                dragSnapToOrigin
+                dragElastic={0.05}
+                dragConstraints={biggerBoxRef}
+                variants={boxVariants}
+                whileHover="hover"
+                whileTap="down"
+            />
+        </BiggerBox>
     );
 }
 
