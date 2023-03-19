76800149e9a48be69aaa561d1fcdf7faa0db391f
RoJaebl <qgs0924@gmail.com>
2023-03-19 20:45:07 +0900

    :hammer: #7.10 fucking crazy animate

--- src/App.tsx
+++ src/App.tsx
@@ -7,64 +7,45 @@ const Wrapper = styled(motion.div)`
     height: 100vh;
     overflow: hidden;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 const Box = styled(motion.div)`
+    width: 400px;
+    height: 400px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 40px;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 1.2em;
-    color: rgb(238, 0, 153);
-    width: 400px;
-    height: 200px;
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 40px;
-    position: absolute;
-    top: 100px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const boxVariants: Variants = {
-    initial: (isBack: boolean) => ({
-        opacity: 0,
-        scale: 0,
-        x: isBack ? -500 : 500,
-    }),
-    animate: { opacity: 1, scale: 1, x: 0, transition: { duration: 1 } },
-    exit: (isBack: boolean) => ({
-        opacity: 0,
-        scale: 0,
-        x: isBack ? 500 : -500,
-        transition: { duration: 1 },
-    }),
-};
+const Circle = styled(motion.div)`
+    background-color: #00a5ff;
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 function App() {
-    const [visible, setVisible] = useState(1);
-    const [isBack, setIsBack] = useState(false);
-    const onNext = () => {
-        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-        setIsBack(false);
-    };
-    const onPrev = () => {
-        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-        setIsBack(true);
-    };
+    const [clicked, setClicked] = useState(false);
+    const toggleClicked = () => setClicked((prev) => !prev);
     return (
-        <Wrapper>
-            <button onClick={onNext}>next</button>
-            <button onClick={onPrev}>prev</button>
-            <AnimatePresence custom={isBack}>
-                <Box
-                    custom={isBack}
-                    key={visible}
-                    variants={boxVariants}
-                    {...boxVariants}
-                >
-                    {visible}
-                </Box>
-            </AnimatePresence>
+        <Wrapper onClick={toggleClicked}>
+            <Box>
+                {!clicked ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked ? (
+                    <Circle
+                        layoutId="circle"
+                        style={{ borderRadius: 0, scale: 1.5 }}
+                    />
+                ) : null}
+            </Box>
         </Wrapper>
     );
 }
